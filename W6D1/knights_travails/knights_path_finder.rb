0e6fe0423ebb2118ec50ed23c7e97a9c1524b91b
Donnie <donald.batryn@gmail.com>
2019-07-02 15:34:14 -0500

    updated .gitignore

--- W6D1/knights_travails/knights_path_finder.rb
+++ W6D1/knights_travails/knights_path_finder.rb
@@ -4,7 +4,7 @@ class KnightPathFinder
     attr_reader :root_node, :considered_positions
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
-        @considered_positions = []
+        @considered_positions = [pos]
         @grid = Array.new(8) {Array.new(8)}
     end
 
@@ -23,28 +23,93 @@ class KnightPathFinder
     end
 
     def build_move_tree
-        @considered_positions = KnightPathFinder::valid_moves(@root_node.value)
-        queue = @considered_positions
+        queue = [@root_node]
         while !queue.empty?
-            new_moves = new_move_positions(queue[0]) 
-            queue += new_moves
-            @considered_positions += new_moves
+            new_nodes = new_move_positions(queue[0]) 
+            new_nodes.each{ |node| @considered_positions << node.value}
+            new_nodes.each {|node| queue += queue[0].children}
             queue.shift
         end
-
-        p "All possible positions a night can go are: #{@considered_positions}"
+        p @considered_positions.sort
     end
 
-    def find_path
-        # find_path is called. Don't write this yet though.
+    def find_path(root, target)
+        # p "find-path root #{root}"
+        return root, [] if root.value == target
+        # p "find-path children #{root.children}"
+    
+        root.children.each do |child| 
+            result, path = find_path(child, target)
+
+            if result != nil
+                #if the result is nil, remove the record
+                return result, trace_back_path(result)
+            else
+                root.remove_child(child)
+                child.parent = nil
+                
+                # erase parent/children
+            end
+        end
+        self.build_move_tree            
+        # always start from root.
+        # does this node exist
+        nil
+
     end
     
-    def new_move_positions(pos)  
-        new_moves = KnightPathFinder::valid_moves(pos)
-        new_moves.select {|move| !@considered_positions.include?(move)}
+    # kpf = KnightPathFinder.new([0, 0])
+    # kpf.find_path([7, 6]) # => [[0, 0], [1, 2], [2, 4], [3, 6], [5, 5], [7, 6]]
+    # kpf.find_path([6, 2]) # => [[0, 0], [1, 2], [2, 0], [4, 1], [6, 2]]
+
+    def trace_back_path(target)
+        path = []
+
+        until target.parent.value == @root_node.value
+            path.unshift(target.value)
+            target = target.parent
+        end
+        path.unshift(target.value)
+        path.unshift(@root_node.value)
+        path
+        # return array of all positions in a chain
+        # unshift each node as it heads back to root
+        # [[0,0], [1,2], [3,3], [5,4]]
+        
+    end
+    
+    def new_move_positions(parent_node)
+        # p "in the new_move #{parent_node}"
+        new_moves = KnightPathFinder::valid_moves(parent_node.value)
+        new_nodes = []
+
+        new_moves.each do |pos|
+            node = PolyTreeNode.new(pos)
+            node.parent = parent_node
+            parent_node.add_child(node)
+            new_nodes << node
+        end
+
+
+        new_nodes.select {|child| !@considered_positions.include?(child.value)}
     end
     
 end
 
-k = KnightPathFinder.new([0, 0])
+k = KnightPathFinder.new([0,0])
 k.build_move_tree
+
+p "find_path is "
+p k.find_path(k.root_node, [4,4])[1]  #works good but the path is not ideal yet
+
+
+# nodes = []
+# for i in 0..5
+#     nodes << PolyTreeNode.new([i,i])
+# end
+# for i in 0..4
+#     nodes[i+ 1].parent = nodes[i]
+#     nodes[i].add_child(nodes[i + 1])
+# end
+
+# p k.trace_back_path # 0,0 1,1 2,2 3,3 4,4
