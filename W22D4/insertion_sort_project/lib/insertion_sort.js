5c933c113b3e150df80653d98b332c20a027fb40
Donnie <donald.batryn@gmail.com>
2019-10-24 17:54:27 -0500

    naive sorts

---
+++ W22D4/insertion_sort_project/lib/insertion_sort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentEl = arr[i];
+        let j;
+        for (j = i - 1; j >= 0 && currentEl < arr[j]; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentEl;
+    }
+    return arr;
+}
+
+module.exports = {ls
+    insertionSort
+};
